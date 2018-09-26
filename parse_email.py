5216ad17276f8d17bf51ce5ec64933ea7182780a
kevin.li <li_zhaoyu@msn.com>
2018-09-26 12:38:26 +0800

    final version

--- parse_email.py
+++ parse_email.py
@@ -1,16 +1,26 @@
 #!/usr/local/bin/python3
+from pdf2image import convert_from_path,convert_from_bytes
+
+from email.header import decode_header
 import imaplib
 import email
 import sys
+import os
+import re
+import requests
+from io import BytesIO
+import logging
 
 
 class Mail(object):
-    user = 'li_zhaoyu@qq.com'
-    password = 'xxsjjeacdnfrcahf'
-    imap = 'imap.qq.com'
+    user = 'invoice@rrs.com'
+    password = 'Haier@2018'
+    imap = 'imap.exmail.qq.com'
 
     def __init__(self):
         self.conn = None
+        self.correct_receiver = False
+        self.to = None
         self.unseen = []
         self.all = []
         try:
@@ -20,7 +30,7 @@ class Mail(object):
             print("登录失败: %s" % e)
             sys.exit(1)
         print("登录成功")
-        self.conn.select()
+        self.conn.select("&UXZO1mWHTvZZOQ-/&kK5O9o9sefs-", readonly=False)
 
     def unseen_mail(self):
         """ 未读邮件 """
@@ -30,7 +40,7 @@ class Mail(object):
             print('未读邮件数量:%s' % len(self.unseen))
             # print(' '.join([str(i) for i in self.unseen]))
 
-    def all_mail(self):
+    def all_mail(self): #暂时不使用
         """ 所有邮件 """
         result, data = self.conn.search(None, 'ALL')
         if result == 'OK':
@@ -39,9 +49,20 @@ class Mail(object):
 
     def parse_header(self, msg):
         data, charset = email.header.decode_header(msg['subject'])[0]
+        charset = charset or 'utf8'
         self.charset = charset
         print("header编码是" + charset)
-        subject = str(data, charset)
+        if type(data) == str:
+            subject = data
+        else:
+            subject = str(data, charset)
+
+        to = email.utils.parseaddr(msg['to'])[1]
+        if re.match(r'\d{11,11}@rrs.com', to):
+            self.correct_receiver = True
+            self.to = to.replace('@rrs.com','')
+        else:
+            self.correct_receiver = False
 
         print("Subject: ", subject)
         print("From: ", email.utils.parseaddr(msg['From'])[1])
@@ -57,6 +78,9 @@ class Mail(object):
         return str(part.get_payload(decode=True), charset)
 
     def parse_body(self, msg):
+        if not self.correct_receiver:
+            return
+
         for part in msg.walk():
             if not part.is_multipart():
                 charset = part.get_charset()
@@ -66,13 +90,38 @@ class Mail(object):
                     fh = email.header.Header(name)
                     fdh = email.header.decode_header(fh)
                     fname = fdh[0][0]
-                    print('附件名:', fname)
+                    print('附件名 before decode:', fname)
+
+                    fileName = part.get_filename()
+
+                    try:
+                        fileName = decode_header(fileName)[0][0].decode(decode_header(fileName)[0][1])
+                    except:
+                        print('do not need to decode filename')
+
+                    print('附件名 afeter decode:', fileName)
+                    if not fileName.endswith('.pdf'):
+                        return
+
+                    if bool(fileName):
+                        filePath = os.path.join('.', 'attachments', fileName)
+                        print(filePath)
+                        fp = open(filePath, 'wb')
+                        fp.write(part.get_payload(decode=True))
+                        fp.close()
+                        image = convert_from_path(filePath)
+                        image[0].save('temp.png')
+                        sfiles={'file': open('temp.png','rb')}
+                        res=requests.post('http://180.76.188.189:8890/api/v1/invoices/invoice/email/qrcode',files=sfiles, data={'mobile': self.to} )
+                        print (res.text)
                 else:
-                    print(self.parse_part_to_str(part))
+                    print('no attachments')
+                    #print(self.parse_part_to_str(part)) # print 邮件正文
 
 
     def parse(self):
-        nums = self.all[1:3]
+        #nums = self.all[1:3]
+        nums = self.unseen
         for num in nums:
             try:
                 result, data = self.conn.fetch(num, '(RFC822)')
@@ -82,8 +131,10 @@ class Mail(object):
                     self.parse_header(msg)
                     print('-'* 20)
                     self.parse_body(msg)
+                    typ, data = self.conn.store(num,'+FLAGS','\\Seen')
             except Exception as e:
                 print('Message %s 解析错误:%s' % (num, e))
+                logging.exception("解析错误")
 
 
     def over(self):
@@ -92,7 +143,7 @@ class Mail(object):
 
     def run(self):
         self.unseen_mail()
-        self.all_mail()
+        #self.all_mail()
         self.parse()
         self.over()
 
